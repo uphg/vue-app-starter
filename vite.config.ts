3fa0ec8c579832252119f21b5a0b61b6eb1db729
lvheng <1834805770@qq.com>
2023-11-06 11:00:18 +0800

    build: module build

--- vite.config.ts
+++ vite.config.ts
@@ -45,6 +45,20 @@ export default defineConfig({
     }),
     UnoCSS(),
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules/naive-ui')) {
+            return 'naive-ui';
+          }
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
